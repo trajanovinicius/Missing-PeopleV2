80bd0381bca66aed6aeb863304fb7f0dc3bd15ab
Rodolfo Henrique <viniciustrajanobs@gmail.com>
2025-03-17 16:18:58 -0300

    init: initial project structure completed. server working

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import "reflect-metadata";
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+	res.send("🚀 PlenoStack API rodando!");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+	console.log(`✅ Servidor rodando na porta ${PORT}`);
+});
