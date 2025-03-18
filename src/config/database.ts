c6df43cd38656289d5b36fc46c0819df20dcacbe
Rodolfo Henrique <viniciustrajanobs@gmail.com>
2025-03-18 20:39:08 -0300

    refactor: replacing database, Mysql with Postgres

--- src/config/database.ts
+++ src/config/database.ts
@@ -5,15 +5,13 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const AppDataSource = new DataSource({
-	type: "mysql",
+	type: "postgres",
 	host: process.env.DB_HOST || "localhost",
-	port: Number(process.env.DB_PORT) || 3306,
-	username: process.env.DB_USER || "root",
-	password: process.env.DB_PASS || "",
+	port: Number(process.env.DB_PORT) || 5432,
+	username: process.env.DB_USER || "postgres",
+	password: process.env.DB_PASS || "password",
 	database: process.env.DB_NAME || "pleno_stack_api",
-	synchronize: true,
-	logging: true,
 	entities: ["src/modules/**/entities/*.ts"],
 	migrations: ["src/database/migrations/*.ts"],
-	subscribers: [],
+	migrationsRun: false,
 });
